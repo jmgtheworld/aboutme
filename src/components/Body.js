009394d8b0f94540e748b387222fa470481ae591
jmgtheworld <avedj94@hotmail.com>
2021-07-01 15:42:02 -0400

    added angular, updated picture, organized skill set

--- src/components/Body.js
+++ src/components/Body.js
@@ -2,129 +2,299 @@ import React from "react";
 import "../components/body.scss";
 
 export default function Body() {
-
   return (
-    <div className = "body" >
+    <div className="body">
       {/* Start of Body  */}
-      <h3 className = "name" id = "about"> Min Goo Jo </h3> 
-      <h4 className = "title"> Full Stack Developer </h4>
-      <h5 className = "aboutMe"> 
-        I enjoy solving problems and learning new ways to approach challenging projects. 
-        With serious passion for developing fast, intuitive, dynamic apps, I love utilizing
-        all full stack development tools available to provide the best user experience.
+      <h3 className="name" id="about">
+        {" "}
+        Min Goo Jo{" "}
+      </h3>
+      <h4 className="title"> Full Stack Developer </h4>
+      <h5 className="aboutMe">
+        I enjoy solving problems and learning new ways to approach challenging
+        projects. With serious passion for developing fast, intuitive, dynamic
+        apps, I love utilizing all full stack development tools available to
+        provide the best user experience.
       </h5>
-      <img className = "body__profileImage" src = "../../images/mgj.jpeg" alt = "MGJ"/>
+      <img
+        className="body__profileImage"
+        src="../../images/mgj.jpeg"
+        alt="MGJ"
+      />
 
       {/* List of Skills */}
-      <h4 className = "bodyTitle"> Skills </h4> 
-      <div className = "body__language">
-        <h5 className = "language"> CSS </h5> 
-          <img className = "body__language--item" src = "../images/css3.png" alt = "css3" />
-          <img className = "body__language--item" src = "../images/sass.png" alt = "sass" />
-          <img className = "body__language--item" src = "../images/bootstrap.png" alt = "bootstrap" />
-        <h5 className = "language"> Javascript </h5> 
-          <img className = "body__language--item" src = "../images/ajax.jpg" alt = "ajax"/>
-          <img className = "body__language--item" src = "../images/logo192.png" alt = "react"/>
-          <img className = "body__language--item" src = "../images/redux.svg" alt = "redux"/>
-          <img className = "body__language--item" src = "../images/nodejs.png" alt = "nodejs" />
-          <img className = "body__language--item"  src="../images/jquery.png" alt = "jquery"/>
-          <img className = "body__language--item"  src="../images/maps.png" alt = "maps"/>
-        <h5 className = "language"> Backend </h5> 
-          <img className = "body__language--item"  src="../images/psql.png" alt = "psql"/>
-          <img className = "body__language--item"  src="../images/apollo.png" alt = "apollographql"/>
-          <img className = "body__language--item"  src="../images/rails.png" alt = "ruby"/>
-          <img className = "body__language--item"  src="../images/php.png" alt = "php"/>
-          <img className = "body__language--item"  src="../images/flask.png" alt = "flask"/>
-        <h5 className = "language"> Testing </h5> 
-          <img className = "body__language--item"  src="../images/eslint.svg" alt = "eslint"/>
-          <img className = "body__language--item"  src="../images/mocha.png" alt = "mocha"/>
-          <img className = "body__language--item"  src="../images/chai.png" alt = "chai"/>
-          <img className = "body__language--item"  src="../images/jest.svg" alt = "jest"/>
-          <img className = "body__language--item"  src="../images/cypress.png" alt = "cypress"/>
-          <img className = "body__language--item"  src="../images/storybook.png" alt = "storybook"/>
+      <h4 className="bodyTitle"> Skills </h4>
+      <div className="body__language">
+        <h5 className="language"> Frontend </h5>
+
+        <img
+          className="body__language--item"
+          src="../images/logo192.png"
+          alt="react"
+        />
+        <img
+          className="body__language--item"
+          src="../images/redux.svg"
+          alt="redux"
+        />
+        <img
+          className="body__language--item"
+          src="../images/angular.png"
+          alt="angular"
+        />
+        <img
+          className="body__language--item"
+          src="../images/maps.png"
+          alt="maps"
+        />
+        <img
+          className="body__language--item"
+          src="../images/nodejs.png"
+          alt="nodejs"
+        />
+        <img
+          className="body__language--item"
+          src="../images/ajax.jpg"
+          alt="ajax"
+        />
+        <img
+          className="body__language--item"
+          src="../images/jquery.png"
+          alt="jquery"
+        />
+        <img
+          className="body__language--item"
+          src="../images/css3.png"
+          alt="css3"
+        />
+        <img
+          className="body__language--item"
+          src="../images/sass.png"
+          alt="sass"
+        />
+        <img
+          className="body__language--item"
+          src="../images/bootstrap.png"
+          alt="bootstrap"
+        />
+
+        <h5 className="language"> Backend </h5>
+        <img
+          className="body__language--item"
+          src="../images/psql.png"
+          alt="psql"
+        />
+        <img
+          className="body__language--item"
+          src="../images/apollo.png"
+          alt="apollographql"
+        />
+        <img
+          className="body__language--item"
+          src="../images/rails.png"
+          alt="ruby"
+        />
+        <img
+          className="body__language--item"
+          src="../images/php.png"
+          alt="php"
+        />
+        <img
+          className="body__language--item"
+          src="../images/flask.png"
+          alt="flask"
+        />
+        <h5 className="language"> Testing </h5>
+        <img
+          className="body__language--item"
+          src="../images/eslint.svg"
+          alt="eslint"
+        />
+        <img
+          className="body__language--item"
+          src="../images/mocha.png"
+          alt="mocha"
+        />
+        <img
+          className="body__language--item"
+          src="../images/chai.png"
+          alt="chai"
+        />
+        <img
+          className="body__language--item"
+          src="../images/jest.svg"
+          alt="jest"
+        />
+        <img
+          className="body__language--item"
+          src="../images/cypress.png"
+          alt="cypress"
+        />
+        <img
+          className="body__language--item"
+          src="../images/storybook.png"
+          alt="storybook"
+        />
       </div>
 
       {/* List of Projects */}
-      <div id = "project">
-        <h4 className = "bodyTitle" > Projects </h4> 
-        <div className = "project" >
-          <div className = "project__box"> 
-              <h3 className = "project__box--name"> Rebu </h3> 
-              <img className = "project__item" src = "images/rebu.png" alt = "tinyapp"/> 
-              <p className = "project__box--context">
-                Designated Driving Service: Get others to drive your car after a night out. 
-              </p> 
-              <a className = "project__box--link1" href="https://github.com/jmgtheworld/Rebu" target = "_blank" rel="noreferrer"> 
-              Visit Github Repo {`>`} 
-              </a> 
-              <a className = "project__box--link2" href="https://www.youtube.com/watch?v=OrW3bbwPvg0&feature=youtu.be&ab_channel=MGJ" target = "_blank" rel="noreferrer"> 
-              Watch Live Demo {`>`} 
-              </a> 
-            </div>
-
-          <div className = "project__box">  
-            <h3 className = "project__box--name"> Scheduler </h3>
-            <img className = "project__item" src = "images/scheduler.png" alt = "scheduler"/> 
-            <p className = "project__box--context">
-              React application that allows users to book and cancel interviews. 
-            </p> 
-            <a className = "project__box--link1" href="https://github.com/jmgtheworld/scheduler" target = "_blank" rel="noreferrer">  
-            Visit Github Repo {`>`}
-            </a> 
-            <a className = "project__box--link2" href="https://www.youtube.com/watch?v=HO4OvsK-MWQ&feature=youtu.be&ab_channel=MGJ" target = "_blank" rel="noreferrer"> 
-              Watch Live Demo {`>`} 
-            </a> 
+      <div id="project">
+        <h4 className="bodyTitle"> Projects </h4>
+        <div className="project">
+          <div className="project__box">
+            <h3 className="project__box--name"> Rebu </h3>
+            <img
+              className="project__item"
+              src="images/rebu.png"
+              alt="tinyapp"
+            />
+            <p className="project__box--context">
+              Designated Driving Service: Get others to drive your car after a
+              night out.
+            </p>
+            <a
+              className="project__box--link1"
+              href="https://github.com/jmgtheworld/Rebu"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Visit Github Repo {`>`}
+            </a>
+            <a
+              className="project__box--link2"
+              href="https://www.youtube.com/watch?v=OrW3bbwPvg0&feature=youtu.be&ab_channel=MGJ"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Watch Live Demo {`>`}
+            </a>
           </div>
 
-          <div className = "project__box"> 
-            <h3 className = "project__box--name"> Jungle </h3> 
-            <img className = "project__item" src = "images/list_of_products.png" alt = "wikiMap"/>
-            <p className = "project__box--context">
-            A mini e-commerce application built with Rails 4.2  
-            </p> 
-            <a className = "project__box--link" href="https://github.com/jmgtheworld/jungle" target = "_blank" rel="noreferrer"> 
-            Visit Github Repo {`>`}
-            </a> 
+          <div className="project__box">
+            <h3 className="project__box--name"> Scheduler </h3>
+            <img
+              className="project__item"
+              src="images/scheduler.png"
+              alt="scheduler"
+            />
+            <p className="project__box--context">
+              React application that allows users to book and cancel interviews.
+            </p>
+            <a
+              className="project__box--link1"
+              href="https://github.com/jmgtheworld/scheduler"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Visit Github Repo {`>`}
+            </a>
+            <a
+              className="project__box--link2"
+              href="https://www.youtube.com/watch?v=HO4OvsK-MWQ&feature=youtu.be&ab_channel=MGJ"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Watch Live Demo {`>`}
+            </a>
           </div>
 
-          <div className = "project__box"> 
-              <h3 className = "project__box--name"> SpaceX Stats </h3> 
-              <img className = "project__item" src = "images/spacex.png" alt = "tinyapp"/> 
-              <p className = "project__box--context">
-                View all the interesting facts about SpaceX - Built with Apollo GraphQL & SpaceX-API
-              </p> 
-              <a className = "project__box--link1" href="https://github.com/jmgtheworld/spaceX_launchStats" target = "_blank" rel="noreferrer"> 
-              Visit Github Repo {`>`} 
-              </a> 
-              <a className = "project__box--link2" href="https://www.youtube.com/watch?v=C_MdEKALY0k&feature=youtu.be&ab_channel=MGJ" target = "_blank" rel="noreferrer"> 
-              Watch Live Demo {`>`} 
-              </a> 
-            </div>
-
-
-          <div className = "project__box"> 
-            <h3 className = "project__box--name"> The Dundies </h3> 
-            <img className = "project__item" src = "images/dundies.png" alt = "tinyapp"/> 
-            <p className = "project__box--context">
-              Search and nominate your favourtie movies and share between friends.
-            </p> 
-            <a className = "project__box--link" href="https://github.com/jmgtheworld/omdb" target = "_blank" rel="noreferrer"> 
-            Visit Github Repo {`>`} 
-            </a> 
+          <div className="project__box">
+            <h3 className="project__box--name"> Jungle </h3>
+            <img
+              className="project__item"
+              src="images/list_of_products.png"
+              alt="wikiMap"
+            />
+            <p className="project__box--context">
+              A mini e-commerce application built with Rails 4.2
+            </p>
+            <a
+              className="project__box--link"
+              href="https://github.com/jmgtheworld/jungle"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Visit Github Repo {`>`}
+            </a>
           </div>
 
-          <div className = "project__box"> 
-            <h3 className = "project__box--name"> Wiki-Map </h3> 
-            <img className = "project__item" src = "images/wikiMap.png" alt = "wikiMap"/>
-            <p className = "project__box--context">
-              A web app that allows users to collaboratively create maps which list multiple "points". 
-            </p> 
-            <a className = "project__box--link1" href="https://github.com/audrey-audrey/wiki-map" target = "_blank" rel="noreferrer"> 
-            Visit Github Repo {`>`}
-            </a> 
-            <a className = "project__box--link2" href="https://www.youtube.com/watch?v=pQOuR3MKOTs&feature=youtu.be&ab_channel=MGJ" target = "_blank" rel="noreferrer"> 
-              Watch Live Demo {`>`} 
-            </a> 
+          <div className="project__box">
+            <h3 className="project__box--name"> SpaceX Stats </h3>
+            <img
+              className="project__item"
+              src="images/spacex.png"
+              alt="tinyapp"
+            />
+            <p className="project__box--context">
+              View all the interesting facts about SpaceX - Built with Apollo
+              GraphQL & SpaceX-API
+            </p>
+            <a
+              className="project__box--link1"
+              href="https://github.com/jmgtheworld/spaceX_launchStats"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Visit Github Repo {`>`}
+            </a>
+            <a
+              className="project__box--link2"
+              href="https://www.youtube.com/watch?v=C_MdEKALY0k&feature=youtu.be&ab_channel=MGJ"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Watch Live Demo {`>`}
+            </a>
+          </div>
+
+          <div className="project__box">
+            <h3 className="project__box--name"> The Dundies </h3>
+            <img
+              className="project__item"
+              src="images/dundies.png"
+              alt="tinyapp"
+            />
+            <p className="project__box--context">
+              Search and nominate your favourtie movies and share between
+              friends.
+            </p>
+            <a
+              className="project__box--link"
+              href="https://github.com/jmgtheworld/omdb"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Visit Github Repo {`>`}
+            </a>
+          </div>
+
+          <div className="project__box">
+            <h3 className="project__box--name"> Wiki-Map </h3>
+            <img
+              className="project__item"
+              src="images/wikiMap.png"
+              alt="wikiMap"
+            />
+            <p className="project__box--context">
+              A web app that allows users to collaboratively create maps which
+              list multiple "points".
+            </p>
+            <a
+              className="project__box--link1"
+              href="https://github.com/audrey-audrey/wiki-map"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Visit Github Repo {`>`}
+            </a>
+            <a
+              className="project__box--link2"
+              href="https://www.youtube.com/watch?v=pQOuR3MKOTs&feature=youtu.be&ab_channel=MGJ"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Watch Live Demo {`>`}
+            </a>
           </div>
 
           {/* <div className = "project__box"> 
@@ -150,11 +320,8 @@ export default function Body() {
             Visit Github Repo {`>`} 
             </a> 
           </div> */}
-
-
         </div>
       </div>
     </div>
-  )
-      
+  );
 }
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,10 +1,9 @@
-import React, { useState } from 'react';
-import emailjs from 'emailjs-com';
+import React, { useState } from "react";
+import emailjs from "emailjs-com";
 
 import "../components/contact.scss";
 
 export default function Contact() {
-
   const [successMessage, setsuccessMessage] = useState("");
   const [errorMessageName, seterrorMessageName] = useState("");
   const [errorMessageEmail, seterrorMessageEmail] = useState("");
@@ -12,33 +11,61 @@ export default function Contact() {
 
   function sendEmail(e) {
     e.preventDefault();
-    if (validateName(e.target.name.value) && validateEmail(e.target.email.value) 
-      && validateMessage(e.target.message.value)) {
-      emailjs.sendForm('service_9zz0gmd', 'template_n7qip5f', e.target, 'user_NRT8Q5KwxxO1ERzvVHXFu')
-        .then((result) => {
+    validateForm(
+      e.target.name.value,
+      e.target.email.value,
+      e.target.message.value
+    );
+    if (
+      errorMessageName !== "" &&
+      errorMessageEmail !== "" &&
+      errorMessageContent === ""
+    ) {
+      console.log(errorMessageName, errorMessageEmail, errorMessageContent);
+      emailjs
+        .sendForm(
+          "service_9zz0gmd",
+          "template_n7qip5f",
+          e.target,
+          "user_NRT8Q5KwxxO1ERzvVHXFu"
+        )
+        .then(
+          (result) => {
             console.log(result);
-        }, (error) => {
+          },
+          (error) => {
             console.log(error);
-        });
-        setsuccessMessage("Email sent successfully!")
-        e.target.reset();
-    } 
+          }
+        );
+      setsuccessMessage("Email sent successfully!");
+      e.target.reset();
+    }
   }
-  
+
+  function validateForm(name, email, message) {
+    if (!name) seterrorMessageName("Please enter your name");
+    else seterrorMessageName("");
+
+    if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email))
+      seterrorMessageEmail("Email format is invalid");
+    else seterrorMessageEmail("");
+
+    if (!message) seterrorMessageContent("Please enter your message");
+    else seterrorMessageContent("");
+  }
+
   function validateName(name) {
     if (!name) {
       seterrorMessageName("Name cannot be blank");
-      return false;
     } else {
       seterrorMessageName("");
       return true;
     }
   }
 
-  function validateEmail (email) {
-    if (!(/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email))){
+  function validateEmail(email) {
+    if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)) {
       seterrorMessageEmail("Email format is invalid");
-      return false;
     } else {
       seterrorMessageEmail("");
       return true;
@@ -48,29 +75,51 @@ export default function Contact() {
   function validateMessage(message) {
     if (!message) {
       seterrorMessageContent("Your Message is blank!");
-      return false;
     } else {
       seterrorMessageContent("");
       return true;
     }
   }
 
-  return(
-    <div className = "contact" id = "contact">
-      <h4 className = "bodyTitle" id = "contactTitle"> Contact </h4> 
-      <form onSubmit = {sendEmail} className = "contact__form">
-        <input id = "name" name = "name" type = "text" placeholder = "Your Name" onSubmit = {(e)=> validateName(e.target.value)} /> 
-        <div id = "errorMessage"> {errorMessageName} </div>
-        <input id = "email" name = "email" type = "text" placeholder = "Your Email" onSubmit = {(e)=> validateEmail(e.target.value)}/> 
-        <div id = "errorMessage"> {errorMessageEmail} </div>
-        <textarea id = "message" name = "message" type = "text" placeholder = "Your Message" onSubmit = {(e)=> validateMessage(e.target.value)}/> 
-        <div id = "errorMessage"> {errorMessageContent} </div>
-        <div id = "stateMessage">
-          <div id = "successMessage"> {successMessage} </div> 
+  return (
+    <div className="contact" id="contact">
+      <h4 className="bodyTitle" id="contactTitle">
+        {" "}
+        Contact{" "}
+      </h4>
+      <form onSubmit={sendEmail} className="contact__form">
+        <input
+          id="name"
+          name="name"
+          type="text"
+          placeholder="Your Name"
+          onSubmit={(e) => validateName(e.target.value)}
+        />
+        <div id="errorMessage"> {errorMessageName} </div>
+        <input
+          id="email"
+          name="email"
+          type="text"
+          placeholder="Your Email"
+          onSubmit={(e) => validateEmail(e.target.value)}
+        />
+        <div id="errorMessage"> {errorMessageEmail} </div>
+        <textarea
+          id="message"
+          name="message"
+          type="text"
+          placeholder="Your Message"
+          onSubmit={(e) => validateMessage(e.target.value)}
+        />
+        <div id="errorMessage"> {errorMessageContent} </div>
+        <div id="stateMessage">
+          <div id="successMessage"> {successMessage} </div>
         </div>
-        <button className = "submit" type = "submit"> Submit </button>
+        <button className="submit" type="submit">
+          {" "}
+          Submit{" "}
+        </button>
       </form>
-    </div> 
+    </div>
   );
-
 }
